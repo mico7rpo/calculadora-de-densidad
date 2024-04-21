5066c3e55f8364b147bd51090f8760bf3df8aa23
Nicolás <nicolasrestrepo845@gmail.com>
2024-04-21 11:08:08 -0500

    calculadoradedensidad

--- calculadora_densidad.py
+++
@@ -1,7 +0,0 @@
-def densidad(masa, volumen):
-    return masa / volumen
-
-masa = int(input("escribe el valor de masa:"))
-volumen = int(input("escribe el valor de volumen:"))
-
-print("la densidad es:", densidad(masa, volumen))
